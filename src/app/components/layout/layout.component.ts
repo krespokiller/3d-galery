1da04baf8ae51a812a975eb1e7bbcc5b7aede560
David Vargas <davidsantiagovargas1234@gmail.com>
2023-01-26 14:51:46 -0500

    Feat: beadcrumb changes & readme plantilla

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
@@ -6,10 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./layout.component.scss']
 })
 export class LayoutComponent implements OnInit {
+  public Route!:string
+  constructor(
+    private Router:Router,
 
-  constructor() { }
+  ) {
+    this.changeBreadcrumb('Home')
+   }
 
   ngOnInit(): void {
+    this.Router.events.subscribe(value=>{
+        if (value instanceof NavigationEnd) {
+          console.log(value);
+          if(value.url=='/home'){
+            this.changeBreadcrumb('Home')
+          }else if(value.url=='/home/contact'){
+            this.changeBreadcrumb('Contact')
+          }else if(value.url=='/list'){
+            this.changeBreadcrumb('3d Models Gallery')
+          }else if(value.url=='/upload'){
+            this.changeBreadcrumb('Upload')
+          }
+        }
+    })
+  }
+
+  changeBreadcrumb(name:string):void{
+    this.Route=name
   }
 
 }
